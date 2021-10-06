7e3a6c58ad2b23d87fb21a43ec9961762d7a9f15
Tejomay Saha <tejomay.saha@publicissapient.com>
2021-10-06 15:43:08 +0530

    reducer added

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,25 +1,60 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputReducer = (state, action) => {
+  let newState;
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      newState = {
+        ...state,
+        value: action.value,
+      };
+      break;
+    case "ON_BLUR":
+      newState = {
+        ...state,
+        isTouched: true,
+      };
+      break;
+    case "RESET_INPUT":
+      newState = initialInputState;
+      break;
+    default:
+      newState = state;
+  }
+  return newState;
+};
 
 const useInput = (validatedInput) => {
-  const [inputVal, setInputValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
-  const isInputValid = validatedInput(inputVal);
-  const hasError = !isInputValid && isTouched;
+  // const [inputVal, setInputValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatchInputEvent] = useReducer(
+    inputReducer,
+    initialInputState
+  );
+
+  const isInputValid = validatedInput(inputState.value);
+  const hasError = !isInputValid && inputState.isTouched;
 
   const onChangeHandler = (e) => {
-    setInputValue(e.target.value);
+    // setInputValue(e.target.value);
+    dispatchInputEvent({ type: "INPUT_CHANGE", value: e.target.value });
   };
   const onInputBlur = (e) => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    dispatchInputEvent({ type: "ON_BLUR" });
   };
 
   const reset = () => {
-    setInputValue("");
-    setIsTouched(false);
+    dispatchInputEvent({ type: "RESET_INPUT" });
   };
 
   return {
-    inputVal,
+    inputVal: inputState.value,
     isInputValid,
     onChangeHandler,
     onInputBlur,
